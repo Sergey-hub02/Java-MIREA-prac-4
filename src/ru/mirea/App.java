3898bae07ecd90fbf5b6ced8eebd7074c498f3a2
Sergey-hub02 <s.park190802@gmail.com>
2021-09-16 17:22:18 +0300

    Задание выполнено: логика взаимодействия с пользователем реализована. Приступаю к отчёту

--- src/ru/mirea/App.java
+++ src/ru/mirea/App.java
@@ -1,15 +1,21 @@
 package ru.mirea;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 
 public class App extends JFrame {
+  /*========== КОНСТАНТЫ ==========*/
   private static final int windowWidth = 350;           // ширина окна
   private static final int windowHeight = 200;          // выстота окна
   private static final String windowTitle = "Real Madrid vs AC Milan";    // заголовок окна
 
   private static final Font windowFont = new Font("Ubuntu Mono", Font.BOLD, 25);  // оформление шрифта
+
+  /*========== ПЕРЕМЕННЫЕ ==========*/
+  private short acMilanScore = 0;
+  private short realMadridScore = 0;
 
 
   /**
@@ -21,11 +27,37 @@ public class App extends JFrame {
     setSize(windowWidth, windowHeight);
     setLayout(new BorderLayout());
 
-    JLabel score = new JLabel("Result: 0 X 0", SwingConstants.CENTER);
+    JLabel score = new JLabel("Result: " + acMilanScore + " X " + realMadridScore, SwingConstants.CENTER);
     JButton acMilan = new JButton("AC Milan");
     JButton realMadrid = new JButton("Real Madrid");
     JLabel lastScorer = new JLabel("Last Scorer: N/A", SwingConstants.CENTER);
     JLabel winner = new JLabel("Winner: DRAW", SwingConstants.CENTER);
+
+    acMilan.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        score.setText("Result: " + (++acMilanScore) + " X " + realMadridScore);
+        lastScorer.setText("Last Scorer: AC Milan");
+        winner.setText("Winner: " + ((acMilanScore > realMadridScore)
+                              ? "AC Milan"
+                              : (acMilanScore == realMadridScore)
+                              ? "DRAW"
+                              : "Real Madrid"));
+      }
+    });
+
+    realMadrid.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        score.setText("Result: " + acMilanScore + " X " + (++realMadridScore));
+        lastScorer.setText("Last Scorer: Real Madrid");
+        winner.setText("Winner: " + ((acMilanScore > realMadridScore)
+                              ? "AC Milan"
+                              : (acMilanScore == realMadridScore)
+                              ? "DRAW"
+                              : "Real Madrid"));
+      }
+    });
 
     score.setFont(windowFont);
     acMilan.setFont(windowFont);
